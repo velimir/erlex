0529be3accbded9177a8699e906d10a64e4799d7
Grigory Starinkin <starinkin@reksoft.ru>
2013-08-06 19:53:34 +0400

    ebin dir added to .gitignore, start using rebar

---
+++ src/erlex_app.erl
@@ -0,0 +1,16 @@
+-module(erlex_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    erlex_sup:start_link().
+
+stop(_State) ->
+    ok.
