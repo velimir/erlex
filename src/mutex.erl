21471a1128db6e40d1301f57bfa2036d1b28015f
Grigory Starinkin <starinkin@reksoft.ru>
2013-08-14 20:22:08 +0400

    added mutex with link approach (+ tests)

---
+++ src/mutex.erl
@@ -0,0 +1,220 @@
+%% Code from
+%%   Erlang Programming
+%%   Francecso Cesarini and Simon Thompson
+%%   O'Reilly, 2008
+%%   http://oreilly.com/catalog/9780596518189/
+%%   http://www.erlangprogramming.org/
+%%   (c) Francesco Cesarini and Simon Thompson
+%%   modified by Grigory Starinkin, 2013
+
+-module(mutex).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+-export([start/0, stop/0]).
+-export([wait/0, signal/0]).
+-export([init/0]).
+
+-ifdef(TEST).
+-export([cs_loop/1, locker_loop/1]).
+-endif.
+
+start() ->
+    process_flag(trap_exit, true),
+    register(mutex, spawn(?MODULE, init, [])).
+
+stop() ->
+    mutex ! stop.
+
+wait() ->
+    mutex ! {wait, self()},
+    receive ok -> ok end.
+
+signal() ->
+    mutex ! {signal, self()}, ok.
+
+init() ->
+    free().
+
+free() ->
+    receive
+        {wait, Pid} ->
+            try
+                %% we have to make sure that process is still exists.
+                link(Pid),
+                Pid ! ok
+            catch
+                %% Is it OK?
+                _:_ -> free()
+            end,
+            busy(Pid);
+        stop ->
+            terminate()
+    end.
+
+busy(Pid) ->
+    receive
+        {signal, Pid} ->
+            unlink(Pid),
+            free();
+        {'EXIT', _Pid, _Reason} ->
+            free()
+    end.
+
+terminate() ->
+    receive
+        {wait, Pid} ->
+            exit(Pid, kill),
+            terminate()
+    after
+        0 -> ok
+    end.
+
+%%%===================================================================
+%%% Tests
+%%% XXX: it's to be reviewed
+%%%===================================================================
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+cs_start(Name) ->
+    register(Name, spawn(mutex, cs_loop, [0])),
+    ok.
+
+cs_stop(Name) ->
+    Name ! stop.
+
+cs_loop(ResCount) ->
+    receive
+        {put, Pid} ->
+            Count = ResCount - 1,
+            Pid ! Count,
+            cs_loop(Count);
+        {take, Pid} ->
+            Count = ResCount + 1,
+            Pid ! Count,
+            cs_loop(Count);
+        stop -> ok
+    end.
+
+cs_take(Name) ->
+    Name ! {take, self()},
+    receive Used -> Used end.
+
+cs_put(Name) ->
+    Name ! {put, self()},
+    receive Used -> Used end.
+
+cs_test_() ->
+    {setup,
+     fun() ->
+             cs_start(cs),
+             cs
+     end,
+     fun(Name) ->
+             cs_stop(Name)
+     end,
+     fun(Name) ->
+             [
+              ?_assertEqual(1, cs_take(Name)),
+              ?_assertEqual(2, cs_take(Name)),
+              ?_assertEqual(1, cs_put(Name)),
+              ?_assertEqual(0, cs_put(Name))
+             ]
+     end
+    }.
+
+start_locker(CsName) ->
+    register(locker, spawn(mutex, locker_loop, [CsName])),
+    ok.
+
+locker_loop(CsName) ->
+    receive
+        {lock, Pid} ->
+            wait(),
+            Pid ! {ok, cs_take(CsName)},
+            locker_loop(CsName);
+        {unlock, Pid} ->
+            signal(),
+            Pid ! {ok, cs_put(CsName)},
+            locker_loop(CsName);
+        {stop, Pid} ->
+            Pid ! ok
+    end.
+
+stop_locker() ->
+    locker ! {stop, self()},
+    receive
+        ok -> ok
+    after
+        1000 -> {error, timeout}
+    end.
+
+lock() ->
+    locker ! {lock, self()},
+    receive
+        {ok, Count} -> Count
+    after
+        1000 -> {error, timeout}
+    end.
+
+unlock() ->
+    locker ! {unlock, self()},
+    receive
+        {ok, Count} -> Count
+    after
+        1000 -> {error, timeout}
+    end.
+
+kill_locker() ->
+    case whereis(locker) of
+        undefined -> {error, locker_not_spawned};
+        Pid when is_pid(Pid) ->
+            exit(Pid, mutex_test)
+    end.
+
+locker_test() ->
+    CsName = cs2,
+    ?assertEqual(ok, cs_start(CsName)),
+    start(),
+    ?assertEqual(ok, start_locker(CsName)),
+    ?assertEqual(1, lock()),
+    ?assertEqual(0, unlock()),
+    ?assertEqual(ok, stop_locker()),
+    stop(),
+    cs_stop(CsName).
+
+mutex_test_() ->
+    {setup,
+     fun() ->
+             start(),
+             CsName = mtcs,
+             cs_start(CsName),
+             CsName
+     end,
+     fun(CsName) ->
+             cs_stop(CsName),
+             stop()
+     end,
+     fun(CsName) ->
+             [
+              fun() ->
+                      start_locker(CsName),
+                      ?assertEqual(ok, wait()),
+                      ?assertEqual(1, cs_take(CsName)),
+                      ?assertEqual({error, timeout}, lock()),
+                      ?assertEqual(0, cs_put(CsName)),
+                      kill_locker(),
+                      signal(),
+                      timer:sleep(1000),
+                      ?assertEqual(ok, wait()),
+                      ?assertEqual(1, cs_take(CsName)),
+                      ?assertEqual(0, cs_put(CsName)),
+                      signal()
+              end
+             ]
+     end
+    }.
+
+-endif.
